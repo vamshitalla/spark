4ad44b570f26edf8c8198222f2566f1861a32e7a
Vamshi <33339050+vamshitalla@users.noreply.github.com>
2017-12-02 23:31:07 -0600

    Initial Checkin

---
+++ SimpleDF/src/main/scala/SimpleDF.scala
@@ -0,0 +1,24 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SQLContext
+
+object SimpleDF {
+
+  def main(args: Array[String]): Unit = {
+
+    val path = "/usr/local/Cellar/apache-spark/2.2.0/libexec/examples/src/main/resources/people.json"
+
+    val conf = new SparkConf().setAppName("Simple DataFrame").setMaster("local[*]")
+
+    val sc = new SparkContext(conf)
+
+    val sqlContext = new SQLContext(sc)
+
+    val df = sqlContext.read.json(path)
+
+    df.show()
+  }
+
+}
+
