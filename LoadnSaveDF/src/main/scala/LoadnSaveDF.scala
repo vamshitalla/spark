9e8eed4a36bbd63f0b538f63e417848a055f035f
Vamshi <33339050+vamshitalla@users.noreply.github.com>
2017-12-03 14:23:45 -0600

    Initial Checkin

---
+++ LoadnSaveDF/src/main/scala/LoadnSaveDF.scala
@@ -0,0 +1,33 @@
+/*
+ Author: Vamshi Talla
+ Date  : 12/03/2017
+ */
+
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+import org.apache.spark.sql.SQLContext
+
+
+object LoadnSaveDF {
+
+  def main(args: Array[String]): Unit = {
+
+    val path = "/usr/local/Cellar/apache-spark/2.2.0/libexec/examples/src/main/resources/users.parquet"
+
+    val conf = new SparkConf().setAppName("Load n Save DF").setMaster("local[*]")
+
+    val sc = new SparkContext(conf)
+
+    val sqlContext = new SQLContext(sc)
+
+    val df = sqlContext.read.load(path)
+
+    df.select("name", "favorite_color").write.save("namesAndFavColors.parquet")
+
+  }
+
+
+}
