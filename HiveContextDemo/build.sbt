d6f408169679d064079bd39f70b5d3bf2a55dc44
Vamshi <33339050+vamshitalla@users.noreply.github.com>
2017-12-03 17:56:15 -0600

    Initial Commit

---
+++ HiveContextDemo/build.sbt
@@ -0,0 +1,11 @@
+name := "HiveContextDemo"
+
+version := "0.1"
+
+scalaVersion := "2.10.7"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "1.6.3",
+  "org.apache.spark" %% "spark-sql" % "1.6.3",
+  "org.apache.spark" %% "spark-hive" % "1.6.3"
+)
---
+++ HiveContextDemo/src/main/scala/HiveContextDemo.scala
@@ -0,0 +1,32 @@
+/*
+ Author: Vamshi Talla
+ Date  : 12/03/2017
+ */
+
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+import org.apache.spark.sql.hive.HiveContext
+
+object HiveContextExample {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setAppName("HiveContext Demo").setMaster("local[*]")
+
+    val sc = new SparkContext(conf)
+
+    val hiveCtx = new org.apache.spark.sql.hive.HiveContext(sc)
+
+    hiveCtx.sql("CREATE TABLE IF NOT EXISTS src(key INT, value STRING)")
+
+    hiveCtx.sql("LOAD DATA LOCAL INPATH '/usr/local/Cellar/apache-spark/2.2.0/libexec/examples/src/main/resources/kv1.txt' OVERWRITE INTO TABLE src")
+
+    hiveCtx.sql("FROM src SELECT key, value").collect().foreach(println)
+
+
+  }
+
+}
