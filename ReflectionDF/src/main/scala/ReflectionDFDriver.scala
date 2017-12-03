b456c7c8e42008bda708e9c48ff84ba933852a14
Vamshi <33339050+vamshitalla@users.noreply.github.com>
2017-12-03 00:23:32 -0600

    Initial Check-in

---
+++ ReflectionDF/src/main/scala/ReflectionDFDriver.scala
@@ -0,0 +1,32 @@
+/*
+ Author: Vamshi Talla
+ Date  : 12/0/2017
+ */
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SQLContext
+
+object ReflectionDFDriver {
+
+  def main(args: Array[String]): Unit = {
+
+    val path = "/usr/local/Cellar/apache-spark/2.2.0/libexec/examples/src/main/resources/people.txt"
+
+    val conf = new SparkConf().setAppName("Reflection DF").setMaster("local[*]")
+
+    val sc = new SparkContext(conf)
+
+    val sqlContext = new SQLContext(sc)
+
+    import sqlContext.implicits._
+
+    val people = sc.textFile(path).map(_.split(",")).map(p => Person(p(0), p(1).trim.toInt)).toDF
+
+    people.registerTempTable("people")
+
+    val teenagers = sqlContext.sql("select name, age from people where age >= 13 and age <= 19").show()
+  }
+
+}
